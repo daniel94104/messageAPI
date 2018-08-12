fecb81d242225216942afeae24255e9057efb981
Daniel Zheng <danielqiaoge@gmail.com>
2018-08-12 15:32:16 -0400

    config for aws

--- messageAPI.py
+++ messageAPI.py
@@ -89,4 +89,4 @@ class SingleMessage(Resource):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)                #  Start a development server
+    app.run(host="0.0.0.0", port=80, debug=True)                #  Start a development server
